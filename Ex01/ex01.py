b1ae564d5b142724de47051af25661732431c97a
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2020-11-10 11:39:08 +0100

    Complete Exercise Sheet 1

--- Ex01/ex01.py
+++ Ex01/ex01.py
@@ -6,42 +6,46 @@ word_frequency_bigram = dict()
 word_frequency_trigram = dict()
 
 # Exercise 1a)
-def process_line(line):
-    # Remove whitespace at the end and beginning of the sentence
-    line = line.strip()
-    # Remove punctuation (Source: https://stackoverflow.com/a/60725620)
-    line = line.translate(str.maketrans('', '', string.punctuation))
-    # Tokenize sentence into a list and add keywords for the beginning and the end of the sentence
-    return ["BOS"] + line.split() + ["EOS"]
+def main():
+    def process_line(line):
+        # Remove whitespace at the end and beginning of the sentence
+        line = line.strip()
+        # Make sentence lowercase
+        line = line.lower()
+        # Remove punctuation (Source: https://stackoverflow.com/a/60725620)
+        line = line.translate(str.maketrans('', '', string.punctuation))
+        # Tokenize sentence into a list and add keywords for the beginning and the end of the sentence
+        return ["[SOS]"] + line.split() + ["[EOS]"]
 
+    processed_corpus = []
+    with open("corpus.txt", "rt") as infile:
+        for line in infile:
+            # Add processed sentence to the list of all sentences in memory
+            processed_corpus += process_line(line)
 
-
-processed_corpus = []
-with open("corpus.txt", "rt") as infile:
-    for line in infile:
-        # Add processed sentence to the list of all sentences in memory
-        processed_corpus += process_line(line)
-
-# Calculate frequency of each word and sequences of up to three words in the corpus
-size_of_processed_corpus = len(processed_corpus)
-for idx, word in enumerate(processed_corpus):
-    # Unigram frequency
-    if word in word_frequency_unigram:
-        word_frequency_unigram[word] += 1
-    else:
-        word_frequency_unigram[word] = 1
-    # Bigram frequency
-    if idx <= size_of_processed_corpus - 2:
-        if (word, processed_corpus[idx + 1]) in word_frequency_bigram:
-            word_frequency_bigram[(word, processed_corpus[idx + 1])] += 1
+    # Calculate frequency of each word and sequences of up to three words in the corpus
+    size_of_processed_corpus = len(processed_corpus)
+    for idx, word in enumerate(processed_corpus):
+        # Unigram frequency
+        if word in word_frequency_unigram:
+            word_frequency_unigram[word] += 1
         else:
-            word_frequency_bigram[(word, processed_corpus[idx + 1])] = 1
-    # Trigram frequency
-    if idx <= size_of_processed_corpus - 3:
-        if (word, processed_corpus[idx + 1], processed_corpus[idx + 2]) in word_frequency_trigram:
-            word_frequency_trigram[(word, processed_corpus[idx + 1], processed_corpus[idx + 2])] += 1
-        else:
-            word_frequency_trigram[(word, processed_corpus[idx + 1], processed_corpus[idx + 2])] = 1
+            word_frequency_unigram[word] = 1
+        # Bigram frequency
+        if idx <= size_of_processed_corpus - 2:
+            if (word, processed_corpus[idx + 1]) in word_frequency_bigram:
+                word_frequency_bigram[(word, processed_corpus[idx + 1])] += 1
+            else:
+                word_frequency_bigram[(word, processed_corpus[idx + 1])] = 1
+        # Trigram frequency
+        if idx <= size_of_processed_corpus - 3:
+            if (word, processed_corpus[idx + 1], processed_corpus[idx + 2]) in word_frequency_trigram:
+                word_frequency_trigram[(word, processed_corpus[idx + 1], processed_corpus[idx + 2])] += 1
+            else:
+                word_frequency_trigram[(word, processed_corpus[idx + 1], processed_corpus[idx + 2])] = 1
+
+if __name__ == "__main__":
+    main()
 
 
 # Exercise 1b)
@@ -70,19 +74,30 @@ def word_prob_trigram(word_i, word_i1, word_i2):
 
 # Exercise 2a)
 def sample_unigram(all_words_prob_unigram):
-    return np.random.choice([*word_frequency_unigram], p=all_words_prob_unigram)
+    return np.random.choice([*word_frequency_unigram],
+                            p=all_words_prob_unigram)
 
 
-def sample_bigram(word_i1, all_words_prob_bigram):
-    return np.random.choice([(word_prev, word_next)
-                             for (word_prev, word_next)
-                             in word_frequency_bigram
-                             if word_prev == word_next],
-                            p=[])
+def sample_bigram(word_i1):
+    matching_bigrams = [(w_i1, w_i)
+                        for (w_i1, w_i)
+                        in word_frequency_bigram
+                        if w_i1 == word_i1]
+    return np.random.choice([w_i for (_, w_i) in matching_bigrams],
+                            p=[word_prob_bigram(w_i, word_i1)
+                               for (_, w_i)
+                               in matching_bigrams])
 
 
-def sample_trigram(all_words_prob_trigram):
-    return np.random.choice([*word_frequency_trigram], p=all_words_prob_trigram)
+def sample_trigram(word_i2, word_i1):
+    matching_trigrams = [(w_i2, w_i1, w_i)
+                         for (w_i2, w_i1, w_i)
+                         in word_frequency_trigram
+                         if w_i2 == word_i2 and w_i1 == word_i1]
+    return np.random.choice([w_i for (_, _, w_i) in matching_trigrams],
+                            p=[word_prob_trigram(w_i, word_i1, word_i2)
+                               for (_, _, w_i)
+                               in matching_trigrams])
 
 
 # Exercise 2b)
@@ -92,75 +107,37 @@ def gen_sentence_unigram():
     sentence = list()
     while True:
         word = sample_unigram(all_words_prob_unigram)
-        if 'EOS' == word:
-            sentence.append('.')
-            return ' '.join(sentence)
+        if word == '[EOS]':
+            del sentence[0]
+            return ' '.join(sentence) + '.'
         else:
             sentence.append(word)
 
 
 def gen_sentence_bigram():
-    all_words_prob_bigram = [word_prob_bigram(word_i, word_i1)
-                             for (word_i1, word_i)
-                             in word_frequency_bigram.keys()]
-
-    i = 0
-    sentence = list()
+    i = 1
+    sentence = ['[SOS]']
     while True:
-        word = sample_bigram(all_words_prob_bigram)
-        if 'EOS' == word:
-            sentence.append('.')
-            return ' '.join(sentence)
+        word = sample_bigram(sentence[i - 1])
+        if word == '[EOS]':
+            del sentence[0]
+            return ' '.join(sentence) + '.'
         else:
             sentence.append(word)
+            i += 1
 
 
 def gen_sentence_trigram():
-    all_words_prob_trigram = [word_prob_trigram(word_i, word_i1, word_i2)
-                              for (word_i2, word_i1, word_i)
-                              in word_frequency_trigram.keys()]
-
-
-print(gen_sentence_unigram())
-
-
-# Example
-# from collections import defaultdict
-# import numpy as np
-#
-# dice3 = defaultdict(int)
-#
-# for observation in [0, 1, 2, 2, 1, 0, 2, 2, 1, 1, 0]:
-#     dice3[observation] += 1
-#
-# vocabulary = list(dice3.keys())
-# values = np.array(list(dice3.values()))
-#
-# print("vocabulary", vocabulary)
-# print("observations", values)
-#
-# distribution = values / values.sum()
-# print("values", distribution, distribution.sum())
-#
-# print("distribution-cumulative", distribution.cumsum())
-#
-# N = 10
-# throws = []
-#
-# def sample(vocabulary, distribution):
-#     dist_sum = distribution.cumsum()
-#     randval = np.random.random()
-#
-#     for index, value in enumerate(dist_sum):
-#         if value < randval:
-#             continue
-#         return index
-#
-#     return len(dist_sum) - 1
-#
-#
-# throws = [sample(vocabulary, distribution) for _ in range(N)]
-# print(throws)
-#
-# throws = [np.random.choice(vocabulary, p=distribution) for _ in range(N)]
-# print(throws)
+    i = 1
+    sentence = ['[SOS]']
+    while True:
+        if i >= 2:
+            word = sample_trigram(sentence[i - 2], sentence[i - 1])
+        if i == 1:
+            word = sample_bigram(sentence[i - 1])
+        if word == '[EOS]':
+            del sentence[0]
+            return ' '.join(sentence) + '.'
+        else:
+            sentence.append(word)
+            i += 1
